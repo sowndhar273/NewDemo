e5d1c1c0fe463d571a538535e7cb2c5793a855b5
sowndhar273 <sowndhar273@gmail.com>
2022-02-03 00:35:51 +0530

    Added sop lines

--- Addition.java
+++ Addition.java
@@ -5,7 +5,9 @@ public static void main(String args[])
 { 
 	int a,b,x;
 	x=a+b;
-	System.out.println("The result is:" +x);
+	System.out.println("Addition of two integer will also produce an integer");
+	System.out.println("The result of two integer values is:" +x);
+	System.out.println("Any operations on integers will produce an integer "); 
 }
 }
 

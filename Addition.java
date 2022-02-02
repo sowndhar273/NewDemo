3f64d43147b27d0cbe3b44f989af79f098cabda0
sowndhar273 <sowndhar273@gmail.com>
2022-02-03 00:42:19 +0530

    Added a new integer in addition process

--- Addition.java
+++ Addition.java
@@ -3,8 +3,8 @@ class Addition
 
 public static void main(String args[])
 { 
-	int a,b,x;
-	x=a+b;
+	int a,b,c,x;
+	x=a+b+c;
 	System.out.println("Addition of two integer will also produce an integer");
 	System.out.println("The result of two integer values is:" +x);
 	System.out.println("Any operations on integers will produce an integer "); 
